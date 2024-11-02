6800227e36b03514b47bf04b0ff5da146ce56eae
jovanlekic12 <lekicjovan1212@gmail.com>
2024-11-02 22:15:32 +0100

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,60 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [educations, setEducations] = useState([
+    {
+      universityName: universityName,
+      universityCity: universityCity,
+      degree: degree,
+      subject: subject,
+      universityFromDate: universityFromDate,
+      universtityToDate: universtityToDate,
+    },
+  ]);
 
+  const [experiences, setExperiences] = useState([
+    {
+      position: position,
+      company: company,
+      companyCity: companyCity,
+      companyFromDate: companyFromDate,
+      companyToDate: companyToDate,
+    },
+  ]);
+
+  //personal infos
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [title, setTitle] = useState("");
+  // const [photo, setPhoto] = useState("");
+  const [adress, setAdress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [description, setDescription] = useState("");
+
+  //education
+  const [universityName, setUniversityName] = useState("");
+  const [universityCity, setUniversityCity] = useState("");
+  const [degree, setDegree] = useState("");
+  const [subject, setSubject] = useState("");
+  const [universityFromDate, setUniversityFromDate] = useState("");
+  const [universtityToDate, setUniverstityToDate] = useState("");
+
+  //experience
+  const [position, setPosition] = useState("");
+  const [company, setCompany] = useState("");
+  const [companyCity, setCompanyCity] = useState("");
+  const [companyFromDate, setCompanyFromDate] = useState("");
+  const [companyToDate, setCompanyToDate] = useState("");
   return (
     <>
+      <header className="header">
+        <h1>CV CREATOR</h1>
+      </header>
+      <main className="main__container"></main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
